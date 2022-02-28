60880bc9e0c525ff0083e9f4aa0eb85c31262b43
Zakeer Sainalabdeen <zsainalabdeen@wtcgroup.com>
2022-02-28 11:37:58 -0800

    Implement RabbitMq, MassTransit Async Communication in Ordering.API -> Event Consume

--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -1,9 +1,12 @@
+using EventBus.Messages.Common;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Ordering.API.EventBusConsumer;
 using Ordering.Application;
 using Ordering.Infrastructure;
 
@@ -24,6 +27,27 @@ namespace Ordering.API
             services.AddApplicationServices(); // This Service created in Application Layer
             services.AddInfrastructureServices(Configuration); // This Service created in Infrastructure Layer 
                                                                // Passing appsettings config for Email and DB Con
+
+            // MassTransit-RabbitMQ Configuration
+            services.AddMassTransit(config => {
+
+                config.AddConsumer<BasketCheckoutConsumer>();
+
+                config.UsingRabbitMq((ctx, cfg) => {
+                    cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+
+                    cfg.ReceiveEndpoint(EventBusConstants.BasketCheckoutQueue, c =>
+                    {
+                        c.ConfigureConsumer<BasketCheckoutConsumer>(ctx);
+                    });
+                });
+            });
+            services.AddMassTransitHostedService();
+
+            //General Config
+            services.AddAutoMapper(typeof(Startup));
+            services.AddScoped<BasketCheckoutConsumer>();
+
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
